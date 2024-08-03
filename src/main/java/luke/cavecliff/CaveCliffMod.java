192d070dba05a55c4d46fe4840a42fe0cbab6700
LukeisStuff <lukeisawesome03@gmail.com>
2024-08-03 15:16:18 -0400

    can scrape copper now

--- src/main/java/luke/cavecliff/CaveCliffMod.java
+++ src/main/java/luke/cavecliff/CaveCliffMod.java
@@ -1,6 +1,11 @@
 package luke.cavecliff;
 
+import luke.cavecliff.entity.EntityGlowSquid;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.data.registry.Registries;
+import net.minecraft.core.entity.SpawnListEntry;
+import net.minecraft.core.enums.EnumCreatureType;
+import net.minecraft.core.world.biome.Biome;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.util.ClientStartEntrypoint;
@@ -12,6 +17,9 @@ public class CaveCliffMod implements ModInitializer, ClientStartEntrypoint, Game
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	@Override
 	public void onInitialize() {
+		for (Biome b : Registries.BIOMES) {
+			b.getSpawnableList(EnumCreatureType.waterCreature).add(new SpawnListEntry(EntityGlowSquid.class, 5));
+		}
 		LOGGER.info("Caves and Cliffs initialized.");
 	}
 
---
+++ src/main/java/luke/cavecliff/mixin/ItemToolAxeMixin.java
@@ -0,0 +1,102 @@
+package luke.cavecliff.mixin;
+
+import luke.cavecliff.CaveCliffBlocks;
+import net.minecraft.core.block.Block;
+import net.minecraft.core.data.tag.Tag;
+import net.minecraft.core.entity.player.EntityPlayer;
+import net.minecraft.core.enums.EnumBlockSoundEffectType;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.item.material.ToolMaterial;
+import net.minecraft.core.item.tool.ItemTool;
+import net.minecraft.core.item.tool.ItemToolAxe;
+import net.minecraft.core.util.helper.Side;
+import net.minecraft.core.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(value = ItemToolAxe.class, remap = false)
+public class ItemToolAxeMixin extends ItemTool {
+
+
+	protected ItemToolAxeMixin(String name, int id, int damageDealt, ToolMaterial toolMaterial, Tag<Block> tagEffectiveAgainst) {
+		super(name, id, damageDealt, toolMaterial, tagEffectiveAgainst);
+	}
+
+	@Override
+	public boolean onUseItemOnBlock(ItemStack itemstack, EntityPlayer entityplayer, World world, int blockX, int blockY, int blockZ, Side side, double xPlaced, double yPlaced) {
+		int blockToScrape = world.getBlockId(blockX, blockY, blockZ);
+		int meta = world.getBlockMetadata(blockX, blockY, blockZ);
+		if (blockToScrape == CaveCliffBlocks.blockCopper.id) {
+			if (meta == 3) {
+				Block scrapedBlock = CaveCliffBlocks.blockCopper;
+				world.playBlockSoundEffect(null, (float) blockX + 0.5f, (float) blockY + 0.5f, (float) blockZ + 0.5f, scrapedBlock, EnumBlockSoundEffectType.MINE);
+				if (!world.isClientSide) {
+					world.setBlockAndMetadataWithNotify(blockX, blockY, blockZ, scrapedBlock.id, 2);
+					itemstack.damageItem(1, entityplayer);
+					entityplayer.swingItem();
+				}
+			}
+			entityplayer.swingItem();
+		}
+		if (blockToScrape == CaveCliffBlocks.blockCopper.id) {
+			if (meta == 2) {
+				Block scrapedBlock = CaveCliffBlocks.blockCopper;
+				world.playBlockSoundEffect(null, (float) blockX + 0.5f, (float) blockY + 0.5f, (float) blockZ + 0.5f, scrapedBlock, EnumBlockSoundEffectType.MINE);
+				if (!world.isClientSide) {
+					world.setBlockAndMetadataWithNotify(blockX, blockY, blockZ, scrapedBlock.id, 1);
+					itemstack.damageItem(1, entityplayer);
+					entityplayer.swingItem();
+				}
+			}
+			entityplayer.swingItem();
+		}
+		if (blockToScrape == CaveCliffBlocks.blockCopper.id) {
+			if (meta == 1) {
+				Block scrapedBlock = CaveCliffBlocks.blockCopper;
+				world.playBlockSoundEffect(null, (float) blockX + 0.5f, (float) blockY + 0.5f, (float) blockZ + 0.5f, scrapedBlock, EnumBlockSoundEffectType.MINE);
+				if (!world.isClientSide) {
+					world.setBlockAndMetadataWithNotify(blockX, blockY, blockZ, scrapedBlock.id, 0);
+					itemstack.damageItem(1, entityplayer);
+					entityplayer.swingItem();
+				}
+			}
+			entityplayer.swingItem();
+		}
+		if (blockToScrape == CaveCliffBlocks.brickCopper.id) {
+			if (meta == 3) {
+				Block scrapedBlock = CaveCliffBlocks.brickCopper;
+				world.playBlockSoundEffect(null, (float) blockX + 0.5f, (float) blockY + 0.5f, (float) blockZ + 0.5f, scrapedBlock, EnumBlockSoundEffectType.MINE);
+				if (!world.isClientSide) {
+					world.setBlockAndMetadataWithNotify(blockX, blockY, blockZ, scrapedBlock.id, 2);
+					itemstack.damageItem(1, entityplayer);
+					entityplayer.swingItem();
+				}
+			}
+			entityplayer.swingItem();
+		}
+		if (blockToScrape == CaveCliffBlocks.brickCopper.id) {
+			if (meta == 2) {
+				Block scrapedBlock = CaveCliffBlocks.brickCopper;
+				world.playBlockSoundEffect(null, (float) blockX + 0.5f, (float) blockY + 0.5f, (float) blockZ + 0.5f, scrapedBlock, EnumBlockSoundEffectType.MINE);
+				if (!world.isClientSide) {
+					world.setBlockAndMetadataWithNotify(blockX, blockY, blockZ, scrapedBlock.id, 1);
+					itemstack.damageItem(1, entityplayer);
+					entityplayer.swingItem();
+				}
+			}
+			entityplayer.swingItem();
+		}
+		if (blockToScrape == CaveCliffBlocks.brickCopper.id) {
+			if (meta == 1) {
+				Block scrapedBlock = CaveCliffBlocks.brickCopper;
+				world.playBlockSoundEffect(null, (float) blockX + 0.5f, (float) blockY + 0.5f, (float) blockZ + 0.5f, scrapedBlock, EnumBlockSoundEffectType.MINE);
+				if (!world.isClientSide) {
+					world.setBlockAndMetadataWithNotify(blockX, blockY, blockZ, scrapedBlock.id, 0);
+					itemstack.damageItem(1, entityplayer);
+					entityplayer.swingItem();
+				}
+			}
+			entityplayer.swingItem();
+		}
+		return false;
+	}
+}
